e8985148fabca989b1ccddaec46325b220d471f8
Elias <e.wambugu192@gmail.com>
2023-04-16 22:01:05 -0700

    first commit

--- src/index.js
+++ src/index.js
@@ -1,13 +1,59 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { ClerkProvider, RedirectToSignIn, SignIn, SignUp, SignedIn, SignedOut } from "@clerk/clerk-react";
+import { BrowserRouter, Routes, Route, useNavigate } from "react-router-dom";
+import ProtectedPage from "./ProtectedPage";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+if (!process.env.REACT_APP_CLERK_PUBLISHABLE_KEY) {
+  throw new Error("Missing Publishable Key");
+}
+const clerkPubKey = process.env.REACT_APP_CLERK_PUBLISHABLE_KEY;
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+const ClerkWithRoutes = () => {
+  const navigate = useNavigate();
+
+  return (
+    <ClerkProvider
+      publishableKey={clerkPubKey}
+      navigate={(to) => navigate(to)}
+    >
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route
+          path="/sign-in/*"
+          element={<SignIn redirectUrl={'/protected'} routing="path" path="/sign-in" />}
+        />
+        <Route
+          path="/sign-up/*"
+          element={<SignUp redirectUrl={'/protected'} routing="path" path="/sign-up" />}
+        />
+        <Route
+          path="/protected"
+          element={
+            <>
+            <SignedIn>
+              <ProtectedPage />
+            </SignedIn>
+             <SignedOut>
+              <RedirectToSignIn />
+           </SignedOut>
+            </>
+          }
+        />
+      </Routes>
+    </ClerkProvider>
+  );
+};
+
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <ClerkWithRoutes />
+    </BrowserRouter>
   </React.StrictMode>
 );
 
